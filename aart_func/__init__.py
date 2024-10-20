0689c09544baee7e77037adefa65fa622a356d24
Alejandro Cardenas Avendano <alejandroc137@gmail.com>
2024-10-20 11:51:30 -0600

    Lennox's astute observation to save trees with a (cold) Tukey function for the visamps

--- aart_func/__init__.py
+++ aart_func/__init__.py
@@ -27,6 +27,7 @@ from scipy.special import ellipkinc as ellipf
 from scipy.special import ellipj
 from scipy.special import elliprj
 from scipy.integrate import cumtrapz,quad
+from scipy import signal
 
 #Required for the wrapper for elliptic integral of the third kind
 #import ctypes
